41bea59722998b3ed5cab457b39723e630c7862d
Hadi Direya <hadisamer1420@gmail.com>
2025-05-18 05:43:17 +0300

    Modifying files

--- lib/screens/profile/booking_details_payment.dart
+++ lib/screens/profile/booking_details_payment.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class DetailBooking extends StatelessWidget {
-
   DateTime now = DateTime.now();
 
-  String formattedDateVerbose = DateFormat('d MMMM yyyy').format(DateTime.now());
+  String formattedDateVerbose = DateFormat(
+    'd MMMM yyyy',
+  ).format(DateTime.now());
 
   @override
   Widget build(BuildContext context) {
@@ -15,9 +16,7 @@ class DetailBooking extends StatelessWidget {
         backgroundColor: Colors.white,
         elevation: 0,
         leading: IconButton(
-          icon: const Icon(Icons.arrow_back,
-            color: Colors.black,
-            size: 35,),
+          icon: const Icon(Icons.arrow_back, color: Colors.black, size: 35),
           onPressed: () {
             Navigator.pop(context);
           },
@@ -25,46 +24,35 @@ class DetailBooking extends StatelessWidget {
         title: const Text(
           'Booking Details',
 
-          style: TextStyle(color: Colors.black
-              ,
-              fontWeight: FontWeight.bold),
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
         ),
       ),
-      body:
-      Padding(
-
+      body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
-
-
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             ClipRRect(
               borderRadius: BorderRadius.circular(10),
               child: Image.asset(
-                'assets/images/payment.png',
+                'assets/images/hotel.png',
                 height: 250,
                 width: double.infinity,
               ),
             ),
             SizedBox(height: 20),
 
-            Text('Hotel/Event photo',
-
+            Text(
+              'Hotel/Event photo',
 
               style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
             ),
-            Text(
-              ' $formattedDateVerbose',
-              style: TextStyle(fontSize: 18),
-            ),
+            Text(' $formattedDateVerbose', style: TextStyle(fontSize: 18)),
             SizedBox(height: 20),
 
             Text(
               'Summary',
-              style: TextStyle(fontSize: 18,
-                  fontWeight: FontWeight.bold
-              ),
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 10),
             _buildOrderSummary('price', '\$35.00'),
@@ -76,8 +64,7 @@ class DetailBooking extends StatelessWidget {
 
             Text(
               'Payment Method',
-              style: TextStyle(fontSize: 18,
-                  fontWeight: FontWeight.bold),
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 10),
             Row(
@@ -86,17 +73,12 @@ class DetailBooking extends StatelessWidget {
                 Spacer(),
 
                 TextButton(
-                  onPressed: () {
-                  },
+                  onPressed: () {},
                   child: Text(
                     'Change',
-                    style: TextStyle(
-                      color: Colors.orange[700],
-                    ),
-
+                    style: TextStyle(color: Colors.orange[700]),
                   ),
                 ),
-
               ],
             ),
             SizedBox(height: 20),
@@ -109,15 +91,21 @@ class DetailBooking extends StatelessWidget {
                   style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
                 ),
                 ElevatedButton(
-                  onPressed: () {
-                  },
+                  onPressed: () {},
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.orange[700],
-                    padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 50),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 15,
+                      horizontal: 50,
+                    ),
                   ),
                   child: Text(
                     'Place Order',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ],
@@ -128,7 +116,11 @@ class DetailBooking extends StatelessWidget {
     );
   }
 
-  Widget _buildOrderSummary(String title, String value, {bool isTotal = false}) {
+  Widget _buildOrderSummary(
+    String title,
+    String value, {
+    bool isTotal = false,
+  }) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 5),
       child: Row(
