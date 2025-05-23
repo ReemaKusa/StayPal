153ab296a54303ce2d24e7210a8f757848a90115
Hadi Direya <93659155+Hadi1J@users.noreply.github.com>
2025-05-23 18:27:25 +0300

    Revert "Nosyba"

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,18 +1,14 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    id("com.google.gms.google-services")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    
-
 }
 
 android {
     namespace = "com.example.staypal"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "29.0.13113456"
-
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -28,7 +24,7 @@ android {
         applicationId = "com.example.staypal"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -45,17 +41,4 @@ android {
 
 flutter {
     source = "../.."
-}
-dependencies {
-    // ✅ Firebase BoM
-    implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
-
-    // ✅ Firebase services (you can add more later)
-    implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.firebase:firebase-firestore")
-    implementation("com.google.firebase:firebase-analytics")
-
-    // ✅ Optional Kotlin/AndroidX helpers
-    implementation("androidx.core:core-ktx:1.16.0")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.0")
 }
