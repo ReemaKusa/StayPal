af0e227e87d530f4d774cc48cb2df49fc5e5792f
Reema Kusa <117101399+ReemaKusa@users.noreply.github.com>
2025-06-06 15:18:47 +0300

    App loader/Splash Screen full code

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ import 'package:staypal/screens/admin/views/hotel_manager_view.dart';
 // import 'package:staypal/screens/admin/views/event_organizer_view.dart';
 // import 'package:staypal/screens/admin/views/event_organizer_view.dart';
 import 'package:staypal/widgets/role_landing_view.dart';
+//import 'package:staypal/screens/appSplash/views/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,8 +30,9 @@ void main() async {
     MultiProvider(
       providers: [
         ChangeNotifierProvider(
-          create: (_) => ProfileViewModel()..loadUserData(),
-        ),
+        create: (_) => ProfileViewModel(),
+  ),
+
         ChangeNotifierProvider(create: (_) => HotelManagerViewModel()),
       ],
       child: const MyApp(),
@@ -57,8 +59,9 @@ class MyApp extends StatelessWidget {
       //home:AuthEntryScreen(),
       //home: AdminDashboard(),
       //home: HotelManagerView(),
-      //home: EventOrganizerView(),
-      home:RoleLandingView(),
+      home: EventOrganizerView(),
+     // home:RoleLandingView(),
+     // home: SplashScreen(),
 
       routes: {
         '/wishlist': (context) => WishListPage(),
