df13c3f2fa36951ecdf5ddafbbc0ae95f7e4a2f7
Reema Kusa <117101399+ReemaKusa@users.noreply.github.com>
2025-06-08 14:14:54 +0300

    Editing Details Folder Name

--- lib/screens/detailsPage/hotel/viewmodels/hotel_details_viewmodel.dart
+++ lib/screens/detailsPage/hotel/viewmodels/hotel_details_viewmodel.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:staypal/screens/search_result/hotel/models/hotel_details_model.dart';
+import 'package:staypal/screens/detailsPage/hotel/models/hotel_details_model.dart';
 
 class HotelDetailsViewModel with ChangeNotifier {
   final HotelDetailsModel model;
