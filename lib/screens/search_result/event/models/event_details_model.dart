e38431eede1ee3503fee5cdcb5163e196d4aeab4
Hadi Direya <hadisamer1420@gmail.com>
2025-06-08 05:28:31 +0300

    showing a card that have remaining ntickets number adding notifyer a,provider

--- lib/screens/search_result/event/models/event_details_model.dart
+++ lib/screens/search_result/event/models/event_details_model.dart
@@ -1,56 +1,9 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
-//
-// class EventDetailsModel {
-//   final String eventId;
-//   final Map<String, dynamic> event;
-//
-//   EventDetailsModel({
-//     required this.eventId,
-//     required this.event,
-//   });
-//
-//   String get name => event['name']?.toString() ?? 'No Name';
-//   String get location => event['location']?.toString() ?? 'No Location';
-//   String get description => event['description']?.toString() ?? 'No Description';
-//   String get details => event['details']?.toString() ?? 'No Details';
-//
-//   List<String> get images {
-//     if (event['images'] is List) {
-//       return List<String>.from(event['images'].whereType<String>());
-//     }
-//     return [];
-//   }
-//
-//   double get price => (event['price'] as num?)?.toDouble() ?? 0.0;
-//   String get formattedPrice => '${price.toStringAsFixed(2)} ₪';
-//
-//   List<String> get highlights {
-//     if (event['highlights'] is List) {
-//       return List<String>.from(event['highlights'].whereType<String>());
-//     }
-//     return [];
-//   }
-//
-//   Timestamp? get date => event['date'] is Timestamp ? event['date'] : null;
-//
-//   String get formattedTime {
-//     if (event['time'] is num) {
-//       final hours = (event['time'] as num).toInt();
-//       final minutes = ((event['time'] as num) - hours) * 60;
-//       return '${hours.toString().padLeft(2, '0')}:${minutes.toInt().toString().padLeft(2, '0')}';
-//     }
-//     return 'No Time';
-//   }
-//
-//   bool get isFavorite => event['isFavorite'] == true;
-// }
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:staypal/models/event_model.dart';
 
 class EventDetailsModel {
   final String eventId;
-  final Map<String, dynamic> event;
+  Map<String, dynamic> event;
 
   EventDetailsModel({
     required this.eventId,
@@ -93,21 +46,20 @@ class EventDetailsModel {
   }
 
   bool get isFavorite => event['isFavorite'] == true;
-
-  /// ✅ Extended fields used for booking
   double get rating => (event['rating'] as num?)?.toDouble() ?? 0.0;
 
   int get ticketsSold => (event['ticketsSold'] as int?) ?? 0;
-
   int get availableTickets {
     final limit = (event['limite'] as int?) ?? 0;
     return limit - ticketsSold;
   }
 
   Timestamp? get createdAt => event['createdAt'] is Timestamp ? event['createdAt'] : null;
-
   String get organizerId => event['organizerId']?.toString() ?? '';
 
+  void updateEvent(Map<String, dynamic> newData) {
+    event.addAll(newData);
+  }
 
   EventModel toEventModel() {
     return EventModel(
